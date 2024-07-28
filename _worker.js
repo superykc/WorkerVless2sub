c056e301fc0b97c2fb30f644520eeb3196262442
superykc <75518584+superykc@users.noreply.github.com>
2024-07-28 10:37:44 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -13,7 +13,7 @@ let addresses = [
 let addressesapi = [
 	//'https://raw.githubusercontent.com/cmliu/WorkerVless2sub/main/addressesapi.txt', //可参考内容格式 自行搭建。
 	//'https://raw.githubusercontent.com/cmliu/WorkerVless2sub/main/addressesipv6api.txt', //IPv6优选内容格式 自行搭建。
-	https://w2stxt.pages.dev/w2ss.txt?token=superykc
+	'https://w2stxt.pages.dev/w2ss.txt?token=superykc',
 ];
 
 // 设置优选地址，不带端口号默认80，noTLS订阅生成
